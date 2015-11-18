c911bcafa0e35964be6c93af9422d3af4c460224
codefellow <milo@dogodigi.net>
2015-11-18 23:00:45 +0100

    Set defaults on routes and add where clause for single object requests

--- controllers/rounds.js
+++ controllers/rounds.js
@@ -1,41 +1,43 @@
 var models = require('../models');
 var express = require('express');
 var util = require('../helpers/util.js');
+var objectAssign = require('object-assign');
 var router = express.Router();
-
-router.route('/api/rounds').all(util.ensureAuthorized).get(function(req, res) {
-  models.Round.findAll({
+var defaults = {
+  attributes: [
+    ['datum', 'date'],
+    ['actueel', 'actual'],
+    ['reden', 'reason'],
+    ['bijzonderheden', 'particularities'],
+  ],
+  include: [{
+    model: models.User,
     attributes: [
-      ['datum', 'date'],
-      ['actueel', 'actual'],
-      ['reden', 'reason'],
-      ['bijzonderheden', 'particularities'],
-    ],
-    include: [{
-      model: models.User,
-      attributes: [
-        ['real_name', 'name'],
-        'credit'
-      ]
-    }, {
-      model: models.Remark,
-      attributes: [
-        ['omschrijving', 'description'],
-        ['bijzonderheid', 'particularity'],
-      ]
-    }]
-  }).then(function(results) {
+      ['real_name', 'name'],
+      'credit'
+    ]
+  }, {
+    model: models.Remark,
+    attributes: [
+      ['omschrijving', 'description'],
+      ['bijzonderheid', 'particularity'],
+    ]
+  }]
+};
+router.route('/api/rounds').all(util.ensureAuthorized).get(function(req, res) {
+  var options = objectAssign({},defaults);
+  models.Round.findAll(defaults).then(function(results) {
     res.json(results);
   });
 });
 
 router.route('/api/round/:id').all(util.ensureAuthorized).get(function(req, res) {
-  models.Round.findOne({
-    include: [models.User, models.Remark],
+  var options = objectAssign({
     where: {
       id: req.params.id
     }
-  }).then(function(result) {
+  }, defaults);
+  models.Round.findOne(options).then(function(result) {
     res.json(result);
   });
 });
